2017e31c23a9a8967f47ae3fb1d95536224cc2e3
Apple <Apple@appledemacbook-pro.local>
2015-10-25 20:37:52 +0800

    Initial Commit

---
+++ PetShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PetShow
+//
+//  Created by Apple on 15/10/25.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PetShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PetShow
+//
+//  Created by Apple on 15/10/25.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
